8715a7be0219154d3de6673dd7d46e08fbad2bb7
Dominik Kocka <Dominik.Kocka@fodmobilitygroup.com>
2023-04-02 15:27:33 +0200

    Add starter projects

---
+++ Rest_Tutorial_CSharp/HelloRestClient/Program.cs
@@ -0,0 +1,6 @@
+﻿var client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:5213/api/hello/");
+var result = await client.GetAsync("sayHello?name=me");
+var content = await result.Content.ReadAsStringAsync();
+Console.WriteLine(content);
+Console.ReadKey();
---
+++ Rest_Tutorial_CSharp/HelloRestService/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace HelloRestService
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("sayHello")]
+        public string SayHello([FromQuery] string name)
+        {
+            return $"Hello: {name}";
+        }
+    }
+}
